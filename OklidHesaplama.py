a6007fc1b0daa7009292b0143d16076fb01c8903
Hamza Kılbaş <83731327+hamzakilbas@users.noreply.github.com>
2024-07-31 18:38:45 +0300

    Create OklidHesaplama.py

---
+++ OklidHesaplama.py
@@ -0,0 +1,22 @@
+import math
+# noktalar listesi
+points = [(1, 2), (3, 4), (5, 6), (7, 8) ]
+
+# mesafe hesaplama fonksiyonu
+def euclideanDistance(point1, point2):
+    x1, y1 = point1
+    x2, y2 = point2
+
+    distance = math.sqrt( (x2-x1)**2 + (y2-y1)**2 )
+    return distance
+
+#mesafe listesi
+distances=[]
+for i in range (len(points)):
+    for j in range(i+1, len(points)):
+        distance = euclideanDistance(points[i], points[j])
+        distances.append(distance)
+
+min_distance = min(distances)
+
+print("Minimum distance: ", min_distance)
